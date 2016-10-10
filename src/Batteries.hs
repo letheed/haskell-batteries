c914b859898882724b7d8b8d06ca2924b7e83b78
Letheed <dev@daweb.se>
2016-10-10 06:01:37 +0200

    new: Batteries prelude

---
+++ src/Batteries.hs
@@ -0,0 +1,12 @@
+module Batteries
+  ( module BatteryExports
+  ) where
+
+import Control.Monad.Batteries   as BatteryExports
+import Data.Char.Batteries       as BatteryExports
+import Data.Either.Batteries     as BatteryExports
+import Data.Function.Batteries   as BatteryExports
+import Data.List.Batteries       as BatteryExports
+import Data.String.Batteries     as BatteryExports
+import Data.String.ANSI          as BatteryExports
+import System.IO.Batteries       as BatteryExports
