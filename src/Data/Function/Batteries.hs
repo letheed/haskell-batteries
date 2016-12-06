1bd28731cadd7eb45136b181fbc760480af64ef5
Letheed <dev@daweb.se>
2016-12-06 17:38:51 +0100

    Data.Function: apply

--- src/Data/Function/Batteries.hs
+++ src/Data/Function/Batteries.hs
@@ -1,6 +1,10 @@
 module Data.Function.Batteries
   ( const2
+  , applyN
   ) where
 
 const2 :: a -> b -> c -> a
 const2 a _ _ = a
+
+applyN :: Int -> (a -> a) -> a -> a
+applyN n f = head . drop n . iterate f
